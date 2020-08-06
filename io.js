e630260b1d66ddb44809b319a2c598e85927aeda
micaiah-effiong <micaiah.effiong@gmail.com>
2020-08-06 23:36:18 +0100

    rename event to avoid confusion and conflict

--- io.js
+++ io.js
@@ -3,7 +3,7 @@ let io = require("socket.io")();
 io.on("connection", (socket) => {
   socket.on("joined", (socketId, peerId) => {
     console.log("socketId", socketId, "peerId", peerId);
-    socket.broadcast.emit("joined", socketId, peerId);
+    socket.broadcast.emit("new-user", socketId, peerId);
   });
 });
 
