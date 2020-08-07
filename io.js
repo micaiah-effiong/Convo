124a0d054a6ad3c279c202fad2b42568a24095f7
micaiah-effiong <micaiah.effiong@gmail.com>
2020-08-07 15:48:30 +0100

    listening to disconnect and reposnding with peerId

--- io.js
+++ io.js
@@ -3,7 +3,13 @@ let io = require("socket.io")();
 io.on("connection", (socket) => {
   socket.on("joined", (socketId, peerId) => {
     console.log("socketId", socketId, "peerId", peerId);
+    socket.$peerId = peerId;
     socket.broadcast.emit("new-user", socketId, peerId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(">> disconnection", socket.$peerId);
+    io.emit("disconnect", socket.id, socket.$peerId);
   });
 });
 
